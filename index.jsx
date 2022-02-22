1ef32fee466044762080394f0de0746a928c650b
Jelte Dirks <j.dirks@outlook.com>
2022-02-22 23:15:07 +0100

    splitting up into components
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- index.jsx
+++ index.jsx
@@ -1,16 +1,33 @@
 import ReactDOM from "react-dom";
 
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="https://placehold.co/200x200" alt=""/>
+        <h1>Digital Artisans</h1>
+      </nav>
+    </header>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>This is karma.</footer>
+  )
+}
+
 function Page() {
   return (
-    <nav>
-      <img src="https://placehold.co/200x200" alt=""/>
-      <h1>Digital Artisans</h1>
+    <>
+      <Header/>
       <ul>
         <li>It's high quality</li>
         <li>It's high value</li>
         <li>It's still, just two people developing with humor</li>
       </ul>
-    </nav>
+      <Footer/>
+    </>
   );
 }
 
