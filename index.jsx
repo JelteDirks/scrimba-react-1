fd05d571c12d37f6d305d9536ddc04df41714053
Jelte Dirks <j.dirks@outlook.com>
2022-02-23 20:20:40 +0100

    some styles using stylesheet
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- index.jsx
+++ index.jsx
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom";
 function Header() {
   return (
     <header>
-      <nav>
+      <nav className="navigation">
         <img src="https://placehold.co/200x200" alt=""/>
         <h1>Digital Artisans</h1>
+        <ul className="menu-items">
+          <li>Home</li>
+          <li>Devs</li>
+          <li>Projects</li>
+        </ul>
       </nav>
     </header>
   );
